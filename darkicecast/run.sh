ea2eb627ef116b7921768b53b6ef9773f47e7512
Leif Lang <leif.lang@ontario.ca>
2025-10-24 12:59:58 -0400

    updated run.sh with logging and exec

--- darkicecast/run.sh
+++ darkicecast/run.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Starting DarkIce and Icecast..."
+bashio::log.info "Starting DarkIce and Icecast..."
+
 # Configure audio input
+bashio::log.info "Setting up audio input source..."
 amixer -c 0 sset 'PCM Capture Source' 'Line'
 
 # Start Icecast in background
+bashio::log.info "Starting icecast server..."
 /usr/bin/icecast2 -c /etc/icecast2/icecast.xml &
 
-# Start DarkIce in foreground (so container stays alive)
-/usr/bin/darkice -c /etc/darkice/darkice.cfg
+# Start DarkIce in foreground (main process)
+bashio::log.info "Starting DarkIce streaming..."
+exec /usr/bin/darkice -c /etc/darkice/darkice.cfg
+
+# let the user know everything should be up and running
+bashio::log.info "DarkIce and Icecast should now be running."
