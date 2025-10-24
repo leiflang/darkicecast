729d3a1b7b855f88200ced892d6b64f34c526ddb
Leif Lang <leif.lang@ontario.ca>
2025-10-24 16:51:55 -0400

    improved readability

--- darkicecast/rootfs/etc/cont-init.d/10-audio.sh
+++ darkicecast/rootfs/etc/cont-init.d/10-audio.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/with-contenv bashio
-bashio::log.info "Configuring audio input..."
 # amixer -c 0 sset 'PCM Capture Source' 'Line'
+bashio::log.info "Listing Audio Devices..."
+bashio::log.info "================================== Audio Device List =================================="
 pactl info
 pactl list sources short
+bashio::log.info "======================================================================================="
+
+bashio::log.info "Configuring audio input..."
